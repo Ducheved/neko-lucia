56c17a9be9c0963b965a9eedff5eaa645cb93aff
Denis Chernov <d3lusion4l.nom4d@gmail.com>
2026-07-18 12:34:12 +0300

    Cleaned up cookie-related code

--- packages/lucia/src/cookie.ts
+++ packages/lucia/src/cookie.ts
@@ -11,59 +11,82 @@ export interface CookieAttributes {
 }
 
 export function serializeCookie(name: string, value: string, attributes: CookieAttributes): string {
-	const keyValueEntries: Array<[string, string] | [string]> = [];
-	keyValueEntries.push([encodeURIComponent(name), encodeURIComponent(value)]);
-	if (attributes?.domain !== undefined) {
-		keyValueEntries.push(["Domain", attributes.domain]);
+	const keyValueEntries: string[] = [];
+
+	keyValueEntries.push(`${encodeURIComponent(name)}=${encodeURIComponent(value)}`);
+
+	if (typeof attributes?.domain === "string") {
+		keyValueEntries.push(`Domain=${attributes.domain}`);
 	}
-	if (attributes?.expires !== undefined) {
-		keyValueEntries.push(["Expires", attributes.expires.toUTCString()]);
+
+	if (attributes?.expires instanceof Date) {
+		keyValueEntries.push(`Expires=${attributes.expires.toUTCString()}`);
 	}
+
 	if (attributes?.httpOnly) {
-		keyValueEntries.push(["HttpOnly"]);
+		keyValueEntries.push("HttpOnly");
 	}
-	if (attributes?.maxAge !== undefined) {
-		keyValueEntries.push(["Max-Age", attributes.maxAge.toString()]);
+
+	if (typeof attributes?.maxAge === "number") {
+		keyValueEntries.push(`Max-Age=${attributes.maxAge.toString()}`);
 	}
-	if (attributes?.path !== undefined) {
-		keyValueEntries.push(["Path", attributes.path]);
+
+	if (typeof attributes?.path === "string") {
+		keyValueEntries.push(`Path=${attributes.path}`);
 	}
-	if (attributes?.sameSite === "lax") {
-		keyValueEntries.push(["SameSite", "Lax"]);
+
+	switch (attributes?.sameSite) {
+		case "strict":
+			keyValueEntries.push("SameSite=Strict");
+			break;
+		case "lax":
+			keyValueEntries.push("SameSite=Lax");
+			break;
+		case "none":
+			keyValueEntries.push("SameSite=None");
+			break;
+		default:
+			// Nothing to do here
+			break;
 	}
-	if (attributes?.sameSite === "none") {
-		keyValueEntries.push(["SameSite", "None"]);
-	}
-	if (attributes?.sameSite === "strict") {
-		keyValueEntries.push(["SameSite", "Strict"]);
-	}
+
 	if (attributes?.secure) {
-		keyValueEntries.push(["Secure"]);
+		keyValueEntries.push("Secure");
 	}
-	return keyValueEntries.map((pair) => pair.join("=")).join("; ");
+
+	return keyValueEntries.join("; ");
 }
+
+const COOKIE_SEPARATOR_RE = /;\s*/;
 
 export function parseCookies(header: string): Map<string, string> {
 	const cookies = new Map<string, string>();
 	const duplicates = new Set<string>();
-	const items = header.split(/;\s*/);
+	const items = header.split(COOKIE_SEPARATOR_RE);
 	for (const item of items) {
-		const separatorIndex = item.indexOf("=");
-		const rawKey = separatorIndex === -1 ? item : item.slice(0, separatorIndex);
-		const rawValue = separatorIndex === -1 ? "" : item.slice(separatorIndex + 1);
-		if (!rawKey) continue;
+		const [rawKey, rawValue = ""] = item.split("=", 2);
+		if (!rawKey) {
+			continue;
+		}
+
 		try {
 			const key = decodeURIComponent(rawKey);
-			if (cookies.has(key) || duplicates.has(key)) {
+			if (duplicates.has(key)) {
+				continue;
+			}
+
+			if (cookies.has(key)) {
 				cookies.delete(key);
 				duplicates.add(key);
 				continue;
 			}
+
 			cookies.set(key, decodeURIComponent(rawValue));
 		} catch {
 			continue;
 		}
 	}
+
 	return cookies;
 }
 
